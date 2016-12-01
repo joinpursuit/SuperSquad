06aec782dfdeaacf0a87dbfd52f51d68b3126111
SimoneGrant <simonegrant001@gmail.com>
2016-12-01 16:32:41 -0500

    Storyboard and User Collection Files

---
+++ KatFish/KatFish/UserDetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  UserDetailViewController.swift
+//  KatFish
+//
+//  Created by Simone on 12/1/16.
+//  Copyright © 2016 Jermaine Kelly. All rights reserved.
+//
+
+import UIKit
+import TwicketSegmentedControl
+
+class UserDetailViewController: UIViewController {
+    
+    @IBOutlet weak var mainUserPhoto: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //segmented control
+        let titles = ["Stats", "Address", "Contact"]
+        let frame = CGRect(x: 5, y: view.frame.height / 2 - 20, width: view.frame.width - 10, height: 40)
+        
+        let segmentedControl = TwicketSegmentedControl(frame: frame)
+        segmentedControl.setSegmentItems(titles)
+        
+        view.addSubview(segmentedControl)
+    }
+
+
+
+
+         
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ViewController: TwicketSegmentedControlDelegate {
+    func didSelect(_ segmentIndex: Int) {
+        print("Selected idex: \(segmentIndex)")
+    }
+}
--- KatFish/KatFish/ViewController.swift
+++ KatFish/KatFish/ViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,  UICollectionViewDelegate, UICollectionViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
     }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reuseIdentifier", for: indexPath) as? UserCollectionViewCell
+        
+        // Configure the cell
+        
+        return cell!
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 
 
 }
